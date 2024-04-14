b84a103e07858d574b565a0db8aa3c8b51375ddb
bocah-tua <163550989+bocah-tua@users.noreply.github.com>
2024-04-15 00:49:07 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+const body = document.body;
+const namaSaya = document.createElement("h4");
+namaSaya.innerHTML = "<marquee>ini web gadungan cuman untuk belajar dan mengisi waktu sendiri hikz :(</marquee>";
+body.append(namaSaya);
